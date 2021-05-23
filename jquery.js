8585bf696308baa06cd55344114183ad629a61b4
wangtao <1182792703@qq.com>
2021-05-23 18:30:02 +0800

    添加find和end方法

--- jquery.js
+++ jquery.js
@@ -1,12 +1,37 @@
-window.jquery = function (selector) {
-    const elements = document.querySelectorAll(selector);
+window.jquery = function (selectorOrArray) {
+    let elements;
+    if(typeof selectorOrArray === 'string'){
+        elements = document.querySelectorAll(selectorOrArray);
+    } else {
+        elements = selectorOrArray;
+    }
     return {
+        oldAPI: selectorOrArray.oldAPI,
         addClass(className){
             for(let i = 0; i < elements.length; i++){
                 elements[i].classList.add(className)
             }
             // return api;//实现链式操作
             return this;//实现链式操作
+        },
+        find(selector){
+            let array = [];
+            for(let i = 0; i < elements.length; i++){
+                // array = array.concat(
+                //     elements[i].querySelectorAll(selector)
+                // )
+                const elements2 = Array.from(
+                    elements[i].querySelectorAll(selector)
+                );
+                array = array.concat(elements2)
+            }
+            array.oldAPI = this;// 旧api
+            return jquery(array);
+        },
+        // 实现api返回上一级
+        end(){
+            // console.log(this.oldAPI)
+            return this.oldAPI;
         }
     }
 }
--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-jquery(".test").addClass('red').addClass('bold');
+// jquery(".test").addClass('red').addClass('bold');
+// console.log(jquery(".test").find(".child"))
+let test = jquery(".test");
+test.addClass("red");
+test.find(".child").addClass("gray").end().addClass("back");
+test.addClass("white");
